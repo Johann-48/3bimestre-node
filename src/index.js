16c4078fdd118dd30d92e86d4ba6978a0721930f
EA - Johann Schultz Bauermann <johann_bauermann@educadventista.org>
2025-09-16 11:52:51 -0300

    atualizacao

--- src/index.js
+++ src/index.js
@@ -16,11 +16,11 @@ app.use(express.json());
 app.get("/", (_req, res) => res.json({ ok: true, service: "API 3º Bimestre" }));
 
 //CREATE: POST /usuarios
-app.post("/usuarios", async (req, res) => {
+app.post("/users", async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const novoUsuario = await prisma.user.create({
-      data: { name, email, password }
+      data: { name, email, password },
     });
 
     res.status(201).json(novoUsuario);
@@ -34,14 +34,69 @@ app.post("/usuarios", async (req, res) => {
 });
 
 //READ: GET /usuarios
-app.get("/usuarios", async (_req, res) => {
+app.get("/users", async (_req, res) => {
   try {
     const usuarios = await prisma.user.findMany({
-      orderBy: { id: "asc" }
+      orderBy: { id: "asc" },
     });
     res.json(usuarios);
   } catch (error) {
     res.status(500).json({ error: "Erro ao listar usuários" });
+  }
+});
+
+// POST /stores body: { name, userId }
+app.post("/stores", async (req, res) => {
+  try {
+    const { name, userId } = req.body;
+    const store = await prisma.store.create({
+      data: { name, userId: Number(userId) },
+    });
+    res.status(201).json(store);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
+// GET /stores/:id -> retorna loja + user (dono) + produtos
+app.get("/stores/:id", async (req, res) => {
+  try {
+    const store = await prisma.store.findUnique({
+      where: { id: Number(req.params.id) },
+      include: { user: true, products: true },
+    });
+    if (!store) return res.status(404).json({ error: "Loja não encontrada" });
+    res.json(store);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
+
+app.put("/stores:id", async (req, res) => {
+  try {
+
+  }
+
+// POST /products body: { name, price, storeId }
+app.post("/products", async (req, res) => {
+  try {
+    const { name, price, storeId } = req.body;
+    const product = await prisma.product.create({
+      data: { name, price: Number(price), storeId: Number(storeId) },
+    });
+    res.status(201).json(product);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
+// GET /products -> inclui a loja e o dono da loja
+app.get("/products", async (req, res) => {
+  try {
+    const products = await prisma.product.findMany({
+      include: { store: { include: { user: true } } },
+    });
+    res.json(products);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
   }
 });
 
